13dcbdc42d3cb31838e5829cda2cae50f839ad74
dwayne brock <119648758+atomicBadger2099@users.noreply.github.com>
2025-02-05 15:21:14 -0500

    Add files via upload

---
+++ format_monkey.py
@@ -0,0 +1,34 @@
+#VARIABLES LIST:SAVE STUFF HERE
+opening = "examples of formatted text:"
+name = "welcome to the python crash course"
+first_name = "Frederick"
+last_name = "the Overly Brave"
+full_name = f"{first_name} {last_name}"
+message = f"I am {full_name.title()}."
+
+
+#PRINTING, PRINTING, MORE PRINTING: USE STUFF HERE
+print("\n")
+print("oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+print(opening.upper())
+print("oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+print("\n")
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("KABOOM! CRASH! LOSS OF CONTROL!")
+print("\n")
+print(full_name)
+print(message)
+print("\n")
+print("\tThis is a Tab.")
+print("\t\tTAB IS A SPACE. It isn't a drink. NOT HERE.")
+print("\t\t\tTell Homer to QUIT HITTING THE TAB BUTTON!!!!!")
+print("\n\tThis is a newline with a tab. ")
+print("\t\tLEARN IT. KNOW IT. LIVE IT.")
+print("\n")
+print("OK. I'VE ANNOYED YOU ENOUGH. Bye.")
+print("\n")
+
+
+exit()
